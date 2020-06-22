c2c1116ea4387e1034e0f30b29da4c1a6eafa8fd
disha03 <44141048+disha03@users.noreply.github.com>
2020-06-23 00:42:09 +0530

    add book url

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
-    path('', views.welcome,name='mainpage'),
+    path('', views.welcome,name="mainpage"),
+    path('books/',views.books,name="books")
 ]
